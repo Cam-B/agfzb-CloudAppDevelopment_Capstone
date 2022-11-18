766426c4136093a46b783f63b8798f9426ce6197
Cam-B <cbickf@googlemail.com>
2022-11-18 12:43:06 -0500

    week 2 done 	modified:   server/djangobackend/settings.py

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 APPEND_SLASH = True
 
 ALLOWED_HOSTS = ["localhost", "localhost:8000",
-                 "afaizan.mybluemix.net"]
+                 "camorg556.mybluemix.net"]
 
 CSRF_TRUSTED_ORIGINS = ['https://angelicagard-8000.theiadocker-3-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/',
                         "http://angelicagard.eu-gb.mybluemix.net",'https://*.127.0.0.1']
